6a6fac835c569543379a88e82c4bce8eefd8819d
LeeJizhan <15jzli1@stu.edu.cn>
2018-12-02 17:04:34 +0800

    添加示例

--- app/src/main/java/com/example/daidaijie/syllabusapplication/mystu/MyStuMainActivity.java
+++ app/src/main/java/com/example/daidaijie/syllabusapplication/mystu/MyStuMainActivity.java
@@ -1,18 +1,56 @@
 package com.example.daidaijie.syllabusapplication.mystu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.daidaijie.syllabusapplication.R;
+import com.example.daidaijie.syllabusapplication.base.BaseActivity;
+import com.example.daidaijie.syllabusapplication.bean.Semester;
+import com.example.daidaijie.syllabusapplication.bean.UserLogin;
+import com.example.daidaijie.syllabusapplication.model.User;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.realm.Realm;
+import io.realm.RealmObject;
+import io.realm.RealmResults;
 
 /**
  * MyStu 主界面
  */
-public class MyStuMainActivity extends AppCompatActivity {
+public class MyStuMainActivity extends BaseActivity {
+
+    @BindView(R.id.tv_text)
+    TextView tv_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mystu_main);
+        ButterKnife.bind(this);
+
+        //获取当前用户的账号密码
+        Realm mRealm = Realm.getDefaultInstance();
+        UserLogin userLogin = mRealm.where(UserLogin.class).findFirst();
+        String userName = userLogin.getUsername();
+        String password = userLogin.getPassword();
+
+        //获取当前学期
+        String year = "";
+        String season = "";
+        RealmResults<Semester> semesters = mRealm.where(Semester.class).findAll();
+        for (Semester semester : semesters) {
+            if (semester.isCurrent() == true) {
+                year = semester.getYearString();
+                season = semester.getSeasonString();
+            }
+        }
+
+        String text = year + ":  " + season;
+        tv_text.setText(text);
+    }
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_mystu_main;
     }
 }
